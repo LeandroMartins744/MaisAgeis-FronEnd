687fb8c7a89d05a20af85b8196e57beebdbe86c0
Leandro Silva <lndmrt6@gmail.com>
2021-03-30 21:36:30 -0300

    atualizacoes

--- project/app/src/main/java/com/maisageis/ocorrencias/ui/login/LoginActivity.kt
+++ project/app/src/main/java/com/maisageis/ocorrencias/ui/login/LoginActivity.kt
@@ -3,14 +3,16 @@ package com.maisageis.ocorrencias.ui.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
+import android.widget.Button
 import com.google.android.material.textview.MaterialTextView
+import com.maisageis.ocorrencias.MainActivity
 import com.maisageis.ocorrencias.R
 import com.maisageis.ocorrencias.ui.cadastro.CadastroActivity
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var labelCadastro: MaterialTextView
+    private lateinit var btnLogin: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,15 +24,19 @@ class LoginActivity : AppCompatActivity() {
 
     private fun initOnClicks() {
         labelCadastro.setOnClickListener {
-            val intent = Intent(
-                this@LoginActivity, CadastroActivity::class.java
-            )
+            val intent = Intent(this@LoginActivity, CadastroActivity::class.java)
             startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 
     private fun initViews() {
         labelCadastro = findViewById(R.id.lblCadastro)
+        btnLogin = findViewById(R.id.btnLoginLogar)
     }
-
 }
