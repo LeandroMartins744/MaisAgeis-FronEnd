1fd2a9e5797daf1b3fe41b7d4ecb3d95e760703f
Leandro Silva <lndmrt6@gmail.com>
2021-04-09 18:01:47 -0300

    login

--- project/app/src/main/java/com/maisageis/ocorrencias/data/util/RetrofitInitializer.kt
+++ project/app/src/main/java/com/maisageis/ocorrencias/data/util/RetrofitInitializer.kt
@@ -1,14 +1,20 @@
 package com.maisageis.ocorrencias.data.util
 
+import okhttp3.Credentials
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInitializer {
-    private val client = OkHttpClient.Builder().build()
+    private val client = OkHttpClient.Builder().build().interceptors(BasicAuthInterceptor("maisageis", "1234"))
+    private lateinit var credentials: String
+
+    fun BasicAuthInterceptor(user: String?, password: String?) {
+        this.credentials = Credentials.basic(user, password)
+    }
 
     private var retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl("https://run.mocky.io/v3/")
+        .baseUrl("http://www.maisageis.kinghost.net/project/api/")
         .addConverterFactory(GsonConverterFactory.create())
         .client(client)
         .build()
